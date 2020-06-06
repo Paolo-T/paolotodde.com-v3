cbe58c1a4f9aa1c4f4c93a22f9ebf0d77ac62ca5
Paolo-T <info@paolotodde.com>
2020-06-07 00:54:10 +0100

    Add hover to home tiles

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,16 @@ import { useSpring } from "react-spring";
 import TransitionPageIn from "../components/TransitionPageIn";
 import TransitionInview from "../components/TransitionInview";
 import shuush_banner_1 from "../images/shuush/shuush_banner_1.png";
-import dc_banner_1 from "../images/three-dc/dc_banner_1.png";
+import dc_banner_1 from "../images/three/three_banner_1.png";
 import classica_banner_1 from "../images/classica/classica_banner_1.png";
 
 function Home() {
     //Fade animation
     const fade = useSpring({ opacity: 1, from: { opacity: 0 } });
+    const centerAbsolute = {
+        top: "50%",
+        left: "40%",
+    };
     return (
         <>
             <SEO
@@ -26,8 +30,21 @@ function Home() {
             <TransitionPageIn>
                 <div className="container md:px-0 mx-auto py-32">
                     <TransitionInview>
-                        <div className="w-full my-24 cursor-pointer">
+                        <div className="w-full my-24 cursor-pointer relative transition duration-500 transform hover:scale-95">
                             <Link to="/shuush">
+                                <h2
+                                    className="absolute z-50 inset-0 content-center"
+                                    style={centerAbsolute}
+                                >
+                                    Shuush!
+                                </h2>
+                                <span
+                                    className="absolute z-50"
+                                    style={{ top: "58%", left: "42.75%" }}
+                                >
+                                    PRODUCT DESIGN
+                                </span>
+                                <span className="absolute z-0 inset-0 transition duration-500 ease-in-out bg-black bg-opacity-0 hover:bg-opacity-75"></span>
                                 <img
                                     src={shuush_banner_1}
                                     className="rounded"
@@ -35,9 +52,23 @@ function Home() {
                             </Link>
                         </div>
                     </TransitionInview>
+
                     <TransitionInview>
-                        <div className="w-full mt-24 cursor-pointer">
-                            <Link to="/classica ">
+                        <div className="w-full my-24 cursor-pointer relative transition duration-500 transform hover:scale-95">
+                            <Link to="/classica">
+                                <h2
+                                    className="absolute z-50 inset-0 content-center"
+                                    style={centerAbsolute}
+                                >
+                                    Classica
+                                </h2>
+                                <span
+                                    className="absolute z-50"
+                                    style={{ top: "58%", left: "42.75%" }}
+                                >
+                                    UI DESIGN
+                                </span>
+                                <span className="absolute z-0 inset-0 transition duration-500 ease-in-out bg-black bg-opacity-0 hover:bg-opacity-75"></span>
                                 <img
                                     src={classica_banner_1}
                                     className="rounded"
@@ -45,9 +76,23 @@ function Home() {
                             </Link>
                         </div>
                     </TransitionInview>
+
                     <TransitionInview>
-                        <div className="w-full my-24 cursor-pointer">
+                        <div className="w-full my-24 cursor-pointer relative transition duration-500 transform hover:scale-95">
                             <Link to="/three">
+                                <h2
+                                    className="absolute z-50 inset-0 content-center"
+                                    style={centerAbsolute}
+                                >
+                                    3DC
+                                </h2>
+                                <span
+                                    className="absolute z-50"
+                                    style={{ top: "55%", left: "50%" }}
+                                >
+                                    BRANDING
+                                </span>
+                                <span className="absolute z-0 inset-0 transition duration-500 ease-in-out bg-black bg-opacity-0 hover:bg-opacity-75"></span>
                                 <img src={dc_banner_1} className="rounded" />
                             </Link>
                         </div>
