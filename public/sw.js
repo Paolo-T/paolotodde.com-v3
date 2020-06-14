fe86bdc4bfab9555af8cd84f9f99d0a3eab8a1c4
Paolo-T <info@paolotodde.com>
2020-06-14 13:13:22 +0100

    Design Update

--- public/sw.js
+++ public/sw.js
@@ -33,7 +33,7 @@ self.__precacheManifest = [
     "url": "framework-9fa3c3527bf76bdd7ce7.js"
   },
   {
-    "url": "styles.cf7e62507628019f1f5f.css"
+    "url": "styles.982198ccdbacaae98ba2.css"
   },
   {
     "url": "styles-b9a0d569d0581be7826e.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5382f00b2d8c5829383b69aa03c9dc4d"
+    "revision": "abc1571817cafebb79ab2a682d46f370"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-01b3f85b4acdf44ec1f7.js"
