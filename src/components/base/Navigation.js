12e452941b69bb63336882351cf1f6b25d801bfd
Paolo-T <info@paolotodde.com>
2020-06-26 16:47:20 +0100

    Design update

--- src/components/base/Navigation.js
+++ src/components/base/Navigation.js
@@ -1,24 +1,24 @@
 import React from "react";
 import logo from "../../images/logo.svg";
+import aboutIcon from "../../images/aboutIcon.svg";
 import { Link } from "gatsby";
 
 function Navigation() {
     return (
         <header className="w-full fixed bg-transparent z-10">
-            <div className="w-9/12 max-w-screen-xl mx-auto flex justify-between pt-12 md:pt-24 ">
+            <nav className="w-9/12 max-w-screen-xl mx-auto flex justify-between pt-12 md:pt-24 ">
                 <Link className="text-primary" to="/">
                     <img className="w-8 md:w-8" src={logo} alt="nasa-logo" />
                 </Link>
 
-                <nav className=" flex justify-between">
-                    <Link
-                        className="inline-block text-xl md:text-2xl text-red-600 leading-normal hover:text-black"
-                        to="/about"
-                    >
-                        about
-                    </Link>
-                </nav>
-            </div>
+                <Link className="text-primary" to="/about">
+                    <img
+                        className="w-8 md:w-8"
+                        src={aboutIcon}
+                        alt="nasa-logo"
+                    />
+                </Link>
+            </nav>
         </header>
     );
 }
