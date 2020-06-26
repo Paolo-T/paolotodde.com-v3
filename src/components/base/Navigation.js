687bfb442d7645637f17e8e12a01ba722d1f9435
Paolo-T <info@paolotodde.com>
2020-06-26 16:58:39 +0100

    Design update

--- src/components/base/Navigation.js
+++ src/components/base/Navigation.js
@@ -7,11 +7,11 @@ function Navigation() {
     return (
         <header className="w-full fixed bg-transparent z-10">
             <nav className="w-9/12 max-w-screen-xl mx-auto flex justify-between pt-12 md:pt-24 ">
-                <Link className="text-primary" to="/">
+                <Link to="/">
                     <img className="w-8 md:w-8" src={logo} alt="nasa-logo" />
                 </Link>
 
-                <Link className="text-primary" to="/about">
+                <Link to="/about">
                     <img
                         className="w-8 md:w-8"
                         src={aboutIcon}
