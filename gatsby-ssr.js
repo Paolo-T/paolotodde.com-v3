328a02f707c62960d5b22f5758baad517945e95a
Paolo-T <info@paolotodde.com>
2020-06-29 08:32:19 +0100

    Tidy

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,19 +2,19 @@
 //     return {element};
 // };
 
-const React = require("react");
+// const React = require("react");
 
-export const onRenderBody = ({ setPostBodyComponents }) => {
-    setPostBodyComponents([
-        <script
-            src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.3.3/TextPlugin.min.js"
-            integrity="sha256-iB3kRFdQB/dxNucv7xSsT4IDh6BndoFfxRBtbXgO5fk="
-            crossorigin="anonymous"
-        ></script>,
-        <script
-            src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r99/three.min.js"
-            integrity="sha256-V0EyXecXmQqSk1QW7iej2g+pzqX02oz78cnceES2ZCs="
-            crossorigin="anonymous"
-        ></script>,
-    ]);
-};
+// export const onRenderBody = ({ setPostBodyComponents }) => {
+//     setPostBodyComponents([
+//         <script
+//             src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.3.3/TextPlugin.min.js"
+//             integrity="sha256-iB3kRFdQB/dxNucv7xSsT4IDh6BndoFfxRBtbXgO5fk="
+//             crossorigin="anonymous"
+//         ></script>,
+//         <script
+//             src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r99/three.min.js"
+//             integrity="sha256-V0EyXecXmQqSk1QW7iej2g+pzqX02oz78cnceES2ZCs="
+//             crossorigin="anonymous"
+//         ></script>,
+//     ]);
+// };
