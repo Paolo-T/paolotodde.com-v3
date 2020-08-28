1dbc558aba0f3864c0b56a739cde47e612ac253c
Paolo-T <info@paolotodde.com>
2020-08-28 22:18:50 +0100

    Add description to classica

--- src/pages/classica.js
+++ src/pages/classica.js
@@ -55,6 +55,56 @@ function Classica() {
                             loading="eager"
                         />
                     ))}
+
+                    <div class="w-9/12 max-w-screen-xl mx-auto md:flex my-32">
+                        <div class="flex-2 mt-24">
+                            <h4 class="font-bold uppercase">Skills applied</h4>
+                            <h4 class="pt-4">
+                                Strategy Concept Product Design UI Design
+                                Prototyping.
+                            </h4>
+                        </div>
+                        <div class="flex-3 pl-48 pr-24">
+                            <h2 class="mb-10">The brief</h2>
+                            <p>
+                                You’re asked to design some materials for a
+                                mobile app to be run on smartphones only. Here
+                                some information on the app itself: The app is
+                                for listening to and learning about classical
+                                music.
+                                <br />
+                                The app monetizes through subscriptions. Some of
+                                the content and features are free for all users,
+                                while some are reserved to subscribers.
+                                <br />
+                                <br />
+                                The target customers are the newbie with
+                                curiosity for the topic, but that so far has
+                                lacked a convenient opportunity to educate
+                                themselves on it; and the long-standing
+                                aficionado that’s looking for a high-quality,
+                                comprehensive library of classical music and
+                                related content to enjoy on the go.
+                                <br />
+                                <br />
+                                There are four sections: <br />
+                                <br />• &nbsp; DISCOVER. Here users are
+                                recommended content,​
+                                <br />• &nbsp; SEARCH​. Here they can search for
+                                content),
+                                <br />
+                                ​• &nbsp; LEARN. This is where users can follow
+                                an educational track and test their knowledge,
+                                <br />• &nbsp; ​PROFILE​. This includes their
+                                history of content consumption, their favorites,
+                                their achievements, and some settings.
+                                <br />
+                                <br />
+                                The main navigation between sections may happen
+                                through a tab bar UI.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </TransitionPageIn>
         </>
