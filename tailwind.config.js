d65a1b6042f79f24380751270ea54ff4b52f9371
Paolo-T <iampaolotodde@gmail.com>
2020-09-24 19:57:29 +0100

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -334,7 +334,7 @@ module.exports = {
         },
         lineHeight: {
             none: "1",
-            tight: "1.15",
+            tight: "1",
             snug: "1.375",
             normal: "1.5",
             relaxed: "1.625",
