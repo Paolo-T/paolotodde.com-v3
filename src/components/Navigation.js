0b620fda906cae57530708d6b9ceeca8b6aa3da4
Paolo-T <iampaolotodde@gmail.com>
2020-09-27 12:05:15 +0100

    Update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,7 @@ import { Link } from "gatsby";
 function Navigation() {
     return (
         <header className="w-full fixed bg-transparent z-10">
-            <nav className="w-/12 md:w-8/12 max-w-screen-xl mx-auto flex justify-between pt-12 md:pt-24">
+            <nav className="w-10/12 md:w-8/12 max-w-screen-xl mx-auto flex justify-between pt-12 md:pt-24">
                 <Link to="/">
                     <img className="w-8 md:w-8" src={logo} alt="nasa-logo" />
                 </Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby";
 import { graphql, useStaticQuery } from "gatsby";
 import { useSpring } from "react-spring";
 import Img from "gatsby-image";
-import ImgSlider from "../components/ImgSlider";
+// import ImgSlider from "../components/ImgSlider";
 import TransitionPageIn from "../components/TransitionPageIn";
 import TransitionInview from "../components/TransitionInview";
 
@@ -14,7 +14,7 @@ function Home() {
     const data = useStaticQuery(graphql`
         query homeImages {
             classicaImg: file(
-                relativePath: { eq: "classica/classica__tile.webp" }
+                relativePath: { eq: "classica/classica__tile-bak.webp" }
             ) {
                 id
                 childImageSharp {
@@ -31,7 +31,9 @@ function Home() {
                     }
                 }
             }
-            petImg: file(relativePath: { eq: "petTime/petTime__tile.webp" }) {
+            petImg: file(
+                relativePath: { eq: "petTime/petTime__tile-bak.webp" }
+            ) {
                 id
                 childImageSharp {
                     fluid(maxWidth: 2880) {
@@ -56,14 +58,10 @@ function Home() {
                 ]}
                 title="Home"
             />
-
             <TransitionPageIn>
-                <div className="w-8/12 md:container md:px-0 mx-auto pt-24 md:pt-40">
-                    {/* <TransitionInview>
-                        <ImgSlider />
-                    </TransitionInview> */}
+                <div className="w-10/12 md:container md:px-0 mx-auto pt-24 md:pt-40">
                     <TransitionInview>
-                        <div className="w-12/12 md:pl-5 md:mb-2 md:pt-32">
+                        <div className="w-12/12 md:pl-5 lg:pl-32 md:mb-2 md:pt-32">
                             <h1 className="text-4xl md:text-6xl lg:text-8xl md:mb-10 lg:mb-24">
                                 Hello, I'm Paolo.
                                 <br />
@@ -73,37 +71,40 @@ function Home() {
                                 &&nbsp;
                                 <br />
                                 <span className="inline">front-end</span>{" "}
-                                <span className="inline">dev</span>
+                                <span className="inline text-yellow-500">
+                                    coder
+                                </span>
+                                .
                             </h1>
                         </div>
                     </TransitionInview>
-
                     <TransitionInview>
                         <Link to="/classica" className="mt-20">
                             <Img
                                 fluid={data.classicaImg.childImageSharp.fluid}
                                 alt="classica app ui design"
-                                className="w-full my-4 md:my-12 cursor-pointer relative transition duration-500 transform hover:scale-98 rounded-lg"
+                                className="w-full my-4 md:my-20 cursor-pointer relative transition duration-500 transform hover:scale-98 rounded-lg"
                                 loading="eager"
                             />
                         </Link>
                     </TransitionInview>
+                    <TransitionInview>
+                        <Link to="/petTime" className="mt-20">
+                            <Img
+                                fluid={data.petImg.childImageSharp.fluid}
+                                alt="Pet Time app ui design"
+                                className="w-full my-4 md:my-20 cursor-pointer relative transition duration-500 transform hover:scale-98 rounded-lg"
+                                loading="eager"
+                            />
+                        </Link>
+                    </TransitionInview>
+
                     <TransitionInview>
                         <Link to="/shuush">
                             <Img
                                 fluid={data.shuushImg.childImageSharp.fluid}
                                 alt="shuush app ui design"
-                                className="w-full my-4 md:my-12 cursor-pointer relative transition duration-500 transform hover:scale-98 rounded-lg"
-                                loading="eager"
-                            />
-                        </Link>
-                    </TransitionInview>
-                    <TransitionInview>
-                        <Link to="/petTime">
-                            <Img
-                                fluid={data.petImg.childImageSharp.fluid}
-                                alt="pet time app ui design"
-                                className="w-full my-4 md:mb-16 cursor-pointer relative transition duration-500 transform hover:scale-98 rounded-lg"
+                                className="w-full my-4 md:my-20 cursor-pointer relative transition duration-500 transform hover:scale-98 rounded-lg"
                                 loading="eager"
                             />
                         </Link>
