daa083f9d6ec52da127e155b3a4f3b733f13b496
Paolo-T <iampaolotodde@gmail.com>
2020-10-03 11:47:00 +0100

    bug fix

--- src/utils/chunkArray.js
+++
@@ -1,9 +0,0 @@
-export default function chunkArray(array, size) {
-    const chunked = [];
-    let index = 0;
-    while (index < array.length) {
-        chunked.push(array.slice(index, size + index));
-        index += size;
-    }
-    return chunked;
-}
