dfd0c68ff00231620f2fe845c0c91be57819793d
Paolo-T <iampaolotodde@gmail.com>
2020-10-10 11:49:49 +0100

    image update

--- src/components/ImgTiles.js
+++ src/components/ImgTiles.js
@@ -40,6 +40,34 @@ function ImgTiles({ gridCol, gap }) {
         <>
             <div className={`grid grid-cols-${gridCol} gap-${gap}`}>
                 <TransitionInview>
+                    <Link to="/petTime" className="mt-20 w-full h-full">
+                        <div className="group w-full cursor-pointer relative transition duration-500 transform hover:scale-98 rounded-lg text-center my-2 md:my-10">
+                            <div
+                                className="text-yellow-500 absolute top-1 z-50 opacity-0 group-hover:opacity-100 transition duration-500 "
+                                style={{
+                                    top: "50%",
+                                    left: "50%",
+                                    transform: "translate(-50%, -40%)",
+                                }}
+                            >
+                                <h1 className="mb-1 text-5xl md:text-9xl">
+                                    Pet Time
+                                </h1>
+                                <p className="text-md md:text-3xl ">
+                                    Pet management app
+                                </p>
+                            </div>
+                            <div className="bg-blackPure absolute inset-0 z-40 opacity-0 group-hover:opacity-75 transition-all duration-1000"></div>
+                            <Img
+                                fluid={data.petImg.childImageSharp.fluid}
+                                alt="Pet Time app ui design"
+                                className=""
+                                loading="eager"
+                            />
+                        </div>
+                    </Link>
+                </TransitionInview>
+                <TransitionInview>
                     <Link to="/shuush">
                         <div className="group w-full cursor-pointer relative transition duration-500 transform hover:scale-98 rounded-lg text-center mt-2 md:my-10">
                             <div
@@ -68,34 +96,6 @@ function ImgTiles({ gridCol, gap }) {
                     </Link>
                 </TransitionInview>
 
-                <TransitionInview>
-                    <Link to="/petTime" className="mt-20 w-full h-full">
-                        <div className="group w-full cursor-pointer relative transition duration-500 transform hover:scale-98 rounded-lg text-center my-2 md:my-10">
-                            <div
-                                className="text-yellow-500 absolute top-1 z-50 opacity-0 group-hover:opacity-100 transition duration-500 "
-                                style={{
-                                    top: "50%",
-                                    left: "50%",
-                                    transform: "translate(-50%, -40%)",
-                                }}
-                            >
-                                <h1 className="mb-1 text-5xl md:text-9xl">
-                                    Pet Time
-                                </h1>
-                                <p className="text-md md:text-3xl ">
-                                    Pet management app
-                                </p>
-                            </div>
-                            <div className="bg-blackPure absolute inset-0 z-40 opacity-0 group-hover:opacity-75 transition-all duration-1000"></div>
-                            <Img
-                                fluid={data.petImg.childImageSharp.fluid}
-                                alt="Pet Time app ui design"
-                                className=""
-                                loading="eager"
-                            />
-                        </div>
-                    </Link>
-                </TransitionInview>
                 <TransitionInview>
                     <Link to="/classica" className="w-full h-full">
                         <div className="group w-full cursor-pointer relative transition duration-500 transform hover:scale-98 rounded-lg text-center mb-2 md:mb-10">
