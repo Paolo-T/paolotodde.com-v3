0c1cc582ae47823412c72dd40479b04573542dfa
Paolo-T <iampaolotodde@gmail.com>
2020-10-15 22:44:45 +0100

    Tile spacing

--- src/components/ImgTiles.js
+++ src/components/ImgTiles.js
@@ -41,7 +41,7 @@ function ImgTiles({ gridCol, gap }) {
             <div className={`grid grid-cols-${gridCol} gap-${gap}`}>
                 <TransitionInview>
                     <Link to="/petTime" className="mt-20 w-full h-full">
-                        <div className="group w-full cursor-pointer relative transition duration-500 transform hover:scale-98 text-center my-2 md:my-8">
+                        <div className="group w-full cursor-pointer relative transition duration-500 transform hover:scale-98 text-center my-2 md:mb-16">
                             <div
                                 className="text-yellow-500 absolute top-1 z-50 opacity-0 group-hover:opacity-100 transition duration-500 "
                                 style={{
@@ -69,7 +69,7 @@ function ImgTiles({ gridCol, gap }) {
                 </TransitionInview>
                 <TransitionInview>
                     <Link to="/shuush">
-                        <div className="group w-full cursor-pointer relative transition duration-500 transform hover:scale-98 text-center md:my-8">
+                        <div className="group w-full cursor-pointer relative transition duration-500 transform hover:scale-98 text-center md:my-16">
                             <div
                                 className="text-yellow-500 absolute top-1 z-50 opacity-0 group-hover:opacity-100 transition duration-500"
                                 style={{
