7f1f4e04d8895045e37704900b5195f00e727933
Paolo-T <iampaolotodde@gmail.com>
2020-11-10 17:33:18 +0000

    minor design update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,7 +8,11 @@ function Navigation() {
         <header className="w-full fixed bg-transparent z-10">
             <nav className="w-10/12 md:w-8/12 max-w-screen-xl mx-auto flex justify-between pt-12 md:pt-24">
                 <Link to="/">
-                    <img className="w-8 md:w-8" src={logo} alt="nasa-logo" />
+                    <img
+                        className="w-8 md:w-8 hover:opacity-50 transition duration-500"
+                        src={logo}
+                        alt="nasa-logo"
+                    />
                 </Link>
 
                 <Link to="/about">
