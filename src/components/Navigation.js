a752dbe37f3c1f5570ae4a8068d0a08ec42322d5
Paolo-T <iampaolotodde@gmail.com>
2020-12-13 22:36:20 +0000

    Design update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,35 +3,34 @@ import logo from "../images/logo.svg";
 import { Link } from "gatsby";
 
 function Navigation() {
-    const pageLocation =
-        typeof window !== "undefined" ? window.location.pathname : "";
+   const pageLocation = typeof window !== "undefined" ? window.location.pathname : "";
 
-    return (
-        <header className="w-full fixed bg-transparent z-10">
-            <nav className="w-10/12 md:w-8/12 max-w-screen-xl mx-auto flex justify-between pt-12 md:pt-24">
-                <Link to="/">
-                    <img className="w-8 md:w-8" src={logo} alt="nasa-logo" />
-                </Link>
-                {pageLocation === "/about" ? (
-                    <Link to="/">
-                        <div className="group flex">
-                            <p className="text-xl md:text-2xl text-grey hover:text-primary transition duration-500 hover:underline pt-3 md:pt-2 align-baseline">
-                                Home
-                            </p>
-                        </div>
-                    </Link>
-                ) : (
-                    <Link to="/about">
-                        <div className="group flex">
-                            <p className="text-xl md:text-2xl text-grey hover:text-primary transition duration-500 hover:underline pt-3 md:pt-2 align-baseline">
-                                About
-                            </p>
-                        </div>
-                    </Link>
-                )}
-            </nav>
-        </header>
-    );
+   return (
+      <header className="w-full fixed bg-transparent z-10">
+         <nav className="w-10/12 md:w-8/12 max-w-screen-xl mx-auto flex justify-between pt-12 md:pt-24">
+            <Link to="/">
+               <img className="w-8 md:w-8" src={logo} alt="nasa-logo" />
+            </Link>
+            {pageLocation === "/about" ? (
+               <Link to="/">
+                  <div className="group flex">
+                     <p className="text-xl md:text-2xl hover:text-primary transition duration-500 hover:underline pt-3 md:pt-2 align-baseline">
+                        Home
+                     </p>
+                  </div>
+               </Link>
+            ) : (
+               <Link to="/about">
+                  <div className="group flex">
+                     <p className="text-xl md:text-2xl text-grey hover:text-primary transition duration-500 hover:underline pt-3 md:pt-2 align-baseline">
+                        About
+                     </p>
+                  </div>
+               </Link>
+            )}
+         </nav>
+      </header>
+   );
 }
 
 export default Navigation;
