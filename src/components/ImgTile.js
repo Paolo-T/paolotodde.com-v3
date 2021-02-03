e43edd555753d614c79160cfb6a53d60a6d0f1a9
Paolo-T <iampaolotodde@gmail.com>
2021-02-03 23:19:05 +0000

    Design update

--- src/components/ImgTile.js
+++ src/components/ImgTile.js
@@ -17,9 +17,9 @@ function ImgTile({ linkTo, href, title, subTitle, cta, tileImg, imgAlt, marginTo
                   // onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
                   // onMouseLeave={() => set({ xys: [0, 0, 1] })}
                   // style={{ transform: props.xys.interpolate(trans) }}
-                  className={`group w-full cursor-pointer relative transition duration-500 transform text-center hover:scale-95 overflow-hidden md:mt-${marginTop} md:mb-${marginBottom} rounded-${
+                  className={`group w-full cursor-pointer relative transition duration-500 transform text-center hover:scale-95 md:mt-${marginTop} md:mb-${marginBottom} rounded-${
                      rounded ? rounded : 0
-                  } `}
+                  } overflow-hidden`}
                >
                   <div
                      className="w-full text-primary absolute top-1 z-50 opacity-0 group-hover:opacity-100 transition duration-500"
@@ -58,7 +58,7 @@ function ImgTile({ linkTo, href, title, subTitle, cta, tileImg, imgAlt, marginTo
                   // onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
                   // onMouseLeave={() => set({ xys: [0, 0, 1] })}
                   // style={{ transform: props.xys.interpolate(trans) }}
-                  className={`group w-full cursor-pointer relative transition duration-500 transform hover:scale-95 text-center overflow-hidden  md:mt-${marginTop} md:mb-${marginBottom} rounded-${
+                  className={`group w-full cursor-pointer relative transition duration-500 transform text-center hover:scale-95 overflow-hidden md:mt-${marginTop} md:mb-${marginBottom} rounded-${
                      rounded ? rounded : 0
                   } `}
                >
@@ -70,14 +70,26 @@ function ImgTile({ linkTo, href, title, subTitle, cta, tileImg, imgAlt, marginTo
                         transform: "translate(-50%, -50%)",
                      }}
                   >
-                     {!isPageNav && (
+                     {!isPageNav ? (
                         <>
-                           <h1 className="mb-0 md:mb-1 text-4xl md:text-6xl lg:text-9xl">{title}</h1>
-                           <p className="text-xs md:text-2xl md:mt-2">{subTitle}</p>
+                           <h1 className="mb-0 md:mb-1 text-5xl md:text-6xl lg:text-8xl">{title}</h1>
+                           <p className="text-sm md:text-xl md:mt-2">{subTitle}</p>
                         </>
-                     )}
+                     ) : null}
+                     {/* <p
+                        className="absolute z-100 text-xs md:text-2xl text-primary leading-6 border-2 border-primary px-4 py-2 rounded md:rounded "
+                        style={{
+                           top: "130%",
+                           left: "50%",
+                           transform: "translate(-50%, -50%)",
+                        }}
+                     >
+                        {cta}
+                     </p> */}
                   </div>
-                  <div className="bg-blackPure absolute inset-0 z-40 opacity-0 group-hover:opacity-75 transition-all duration-1000"></div>
+
+                  <div className="bg-blackPure absolute inset-0 z-40 opacity-0 group-hover:opacity-90 transition-all duration-500"></div>
+
                   <Img fluid={tileImg} alt={imgAlt} loading="eager" />
                </div>
             </a>
