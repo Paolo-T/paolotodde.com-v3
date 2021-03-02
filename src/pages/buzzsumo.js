840b95060efae7e92f047b6a0d71b1db2cb38a3e
Paolo-T <iampaolotodde@gmail.com>
2021-03-02 19:15:14 +0000

    Visual update

--- src/pages/buzzsumo.js
+++ src/pages/buzzsumo.js
@@ -71,37 +71,37 @@ function classNameica() {
                      <div class="grid grid-cols-1 md:grid-cols-12 gap-y-10 md:gap-y-14 gap-x-0">
                         {/* Paragraph */}
                         <div className="hidden md:block md:mt-14 col-start-1 col-end-4">
-                           <h4 className="font-sansTitle text-sm uppercase tracking-widest">Skills applied</h4>
+                           <h4 className=" text-sm uppercase tracking-widest">Skills applied</h4>
                            <h4 className="text-sm pt-4">
                               Branding
                               <br /> Photo manipulation
                            </h4>
                         </div>
                         <div className="mt-10 md:mt-0 col-start-5 col-end-13">
-                           <h2 className="font-sansTitle text-3xl mb-3 md:mb-4">Description</h2>
+                           <h2 className=" text-3xl mb-3 md:mb-4">Description</h2>
                            <p>
-                              <span className="font-sansTitle">BuzzSumo</span> Creation of blog image headers to express
-                              BuzzSumo's branding identity. The tone of voice is playful and informal.
+                              <span className="">BuzzSumo</span> Creation of blog image headers to express BuzzSumo's
+                              branding identity. The tone of voice is playful and informal.
                            </p>
                         </div>
                         {/* Paragraph */}
                         <div className="hidden md:block md:mt-14 col-start-1 col-end-4">
-                           <h4 className="font-sansTitle text-sm uppercase tracking-widest">Images</h4>
+                           <h4 className=" text-sm uppercase tracking-widest">Images</h4>
                         </div>
                         <div className="col-start-5 col-end-13">
-                           <h2 className="font-sansTitle text-3xl mb-3 md:mb-4">Blog Images</h2>
+                           <h2 className=" text-3xl mb-3 md:mb-4">Blog Images</h2>
                            <p>
-                              <span className="font-sansTitle">BuzzSumo</span> is a platform that allows you to find
-                              engaging pieces of content and discover new potential outreach opportunities. The site
-                              allows you to search for content that has received the most shares, links and comments.
+                              <span className="">BuzzSumo</span> is a platform that allows you to find engaging pieces
+                              of content and discover new potential outreach opportunities. The site allows you to
+                              search for content that has received the most shares, links and comments.
                            </p>
                         </div>
                         {/* Paragraph */}
                         <div className="hidden md:block md:mt-14 col-start-1 col-end-4">
-                           <h4 className="font-sansTitle text-sm uppercase tracking-widest">Live</h4>
+                           <h4 className=" text-sm uppercase tracking-widest">Live</h4>
                         </div>
                         <div className="col-start-5 col-end-13">
-                           <h2 className="font-sansTitle text-3xl mb-3 md:mb-4">See it live</h2>
+                           <h2 className=" text-3xl mb-3 md:mb-4">See it live</h2>
                            <a
                               href="https://buzzsumo.com/blog/"
                               target="_blank"
@@ -129,14 +129,14 @@ function classNameica() {
                   </div>
                </section>
 
-               <section className="w-11/12 md:w-8/12 lg:w-8/12 xl:w-7/12 max-w-screen-lg mx-auto text-dark mt-24 ">
+               <section className="w-11/12 md:w-8/12 lg:w-8/12 xl:w-7/12 max-w-screen-lg mx-auto text-dark mt-52">
                   <div class="grid grid-cols-1 md:grid-cols-12 gap-y-10 md:gap-y-14 gap-x-0 mb-14 md:mb-32">
                      {/* Paragraph */}
                      <div className="hidden md:block md:mt-14 col-start-1 col-end-4">
-                        <h4 className="font-sansTitle text-sm uppercase tracking-widest">Live</h4>
+                        <h4 className=" text-sm uppercase tracking-widest">Live</h4>
                      </div>
                      <div className="col-start-5 col-end-13">
-                        <h2 className="font-sansTitle text-3xl mb-3 md:mb-4">Web page</h2>
+                        <h2 className=" text-3xl mb-3 md:mb-4">Web page</h2>
                         <p>How the image come together in the grid on Buzzsumo's blog page.</p>
                      </div>
                   </div>
